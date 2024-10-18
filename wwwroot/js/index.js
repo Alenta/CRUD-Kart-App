924ce8c773b4a4175b36fce2b2027fa8b6633faa
Alenta <mef082024@gmail.com>
2024-10-18 10:17:58 +0200

    Javascript for button generation

--- wwwroot/js/index.js
+++ wwwroot/js/index.js
@@ -1,7 +1,24 @@
 
 var state = false;
+var index = 0;
 window.ToggleSideOverlay = ToggleSideOverlay;
 window.DeleteParentElement = DeleteParentElement;
+CreateCard();
+
+function CreateCard(){
+    index++;
+    const current_element = document.getElementById("mastercardcontainer");
+    const clonearea = document.getElementById("cardholder");
+    const clone = current_element.cloneNode(true);
+    clone.id = "removeCard_"+index;
+    clone.querySelector("#cardAddress_0").id = "cardAddress_"+index;
+    clone.querySelector("#cardDD_0").id = "cardDD_"+index;
+    clone.querySelector("#cardCoord_0").id = "cardCoord_"+index;
+    clone.querySelector("#cardupdate1_0").id = "cardupdate1_"+index;
+    clone.querySelector("#cardupdate2_0").id = "cardupdate2_"+index;
+    clone.querySelector("#cardupdate3_0").id = "cardupdate3_"+index;
+    clonearea.appendChild(clone);
+}
 
 function ToggleSideOverlay() {
     if(state===false) {
@@ -14,8 +31,8 @@ function ToggleSideOverlay() {
     }
 }
 
-function DeleteParentElement(ID) {
-    console.log("fishy");
-    let parentID = this.parent.parent.ID;
+function DeleteParentElement(obj) {
+    
+    string = "removeCard_"+obj.id;
     document.getElementById(parentID).remove()
 }
