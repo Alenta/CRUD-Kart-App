d8276eb206e74150ca14f1ee6cc8ac421b86e2d3
SAM082024 <sam082024@gmail.com>
2024-10-22 12:21:18 +0200

    changed files

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        //==============SERVER SETUP=============//
+        //*==============SERVER SETUP=============//!
         var builder = WebApplication.CreateBuilder(args);
     
         // Add services to the container.
@@ -25,21 +25,20 @@ public class Program
         }
         app.UseDefaultFiles();
         app.UseStaticFiles();
-        //app.UseHttpsRedirection(); <-- Gives warning for now. Needed?
-        //app.UseRouting(); Dunno?
+        //app.UseHttpsRedirection(); <-- Gives warning for now. Needed? *Automatically redirect HTTP requests to HTTPS*
+        app.UseRouting(); // Responsible for routing incoming requests to the appropriate endpoint based on the request's path and HTTP method
         //app.UseAuthorization(); Dunno?
         //app.MapRazorPages(); Dunno?
-        app.Run();
 
-        //====================END POINTS===================//
-        //app.MapGet("/health", () => "Server status: OK");
+
+        //*====================END POINTS===================//
+        app.MapGet("/health", () => "Server status: OK");
 
         app.MapGet("/geocode", async (string address, IHttpClientFactory httpClientFactory) =>
         {
             var httpClient = httpClientFactory.CreateClient();
-            var apiKey = "my-api-key"; //The api key
+            var apiKey = "AIzaSyBS7Yyd9cTfgh4Tee84AvjJBm-E2gmnfyE"; //The api key
             var url = $"https://maps.googleapis.com/maps/api/geocode/json?address={Uri.EscapeDataString(address)}&key={apiKey}";
-
             var response = await httpClient.GetAsync(url);
             if (!response.IsSuccessStatusCode)
             {
@@ -58,23 +57,21 @@ public class Program
             return Results.NotFound("Address not found");
         });
 
-        //When a user inputs a search
+        // When a user inputs a search
         app.MapGet("/maps/search/", () => "");
 
-        //When a user clicks on a saved pin
+        // When a user clicks on a saved pin
         app.MapGet("/pin/", () => "");
 
-        //When a user shows detailed info on pin
+        // When a user shows detailed info on pin
         app.MapGet("/pinDetailed/", () => "");
 
-        //When a user creates a new pin
+        // When a user creates a new pin
         app.MapPost("/newPin", () => "");
 
-        //When a user deletes an old pin
+        // When a user deletes an old pin
         app.MapDelete("/deletePin", ()=> "");
 
-        
-        
-
+        app.Run(); //! Must be at the bottom to be able to read app functions
     }
 }
